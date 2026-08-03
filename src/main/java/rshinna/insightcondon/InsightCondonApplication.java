f3e622eee608d2f1b507e8efa6e2f2c021f3dde2
Rodrigo Franco <rshina@hotmail.com>
2026-08-03 20:15:27 -0300

    chore: adiciona classe principal e estrutura de testes

---
+++ src/main/java/rshinna/insightcondon/InsightCondonApplication.java
@@ -0,0 +1,13 @@
+package rshinna.insightcondon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightCondonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsightCondonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rshinna/insightcondon/InsightCondonApplicationTests.java
@@ -0,0 +1,13 @@
+package rshinna.insightcondon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightCondonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
