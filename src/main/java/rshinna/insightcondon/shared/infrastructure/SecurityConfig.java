d8332123e65a06f8371e2dd197367ab32fade5db
Rodrigo Franco <rshina@hotmail.com>
2026-08-05 19:35:21 -0300

    chore: configura Security stateless temporário, libera endpoints condominio e usuario

---
+++ src/main/java/rshinna/insightcondon/shared/infrastructure/SecurityConfig.java
@@ -0,0 +1,36 @@
+package rshinna.insightcondon.shared.infrastructure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .csrf(csrf -> csrf.disable())
+
+                .formLogin(form -> form.disable())
+
+                .httpBasic(basic -> basic.disable())
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/condominios/**").permitAll()
+                        .requestMatchers("/api/usuarios/**").permitAll()
+
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
